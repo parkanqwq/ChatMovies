ba2cd734047f1817324b21dc6d2b2c9e03c153a5
bfmax <no>
2022-02-11 19:43:52 +0300

    подготовка проекта к работе. Библиотеки и папки

---
+++ app/src/main/java/com/metelev/bos/chatmovies/App.kt
@@ -0,0 +1,18 @@
+package com.metelev.bos.chatmovies
+
+import android.app.Application
+import com.metelev.bos.chatmovies.di.retrofitModule
+import com.metelev.bos.chatmovies.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.GlobalContext
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        GlobalContext.startKoin {
+            androidContext(this@App)
+            modules(viewModelModule, retrofitModule)
+        }
+    }
+}
---
+++ app/src/main/java/com/metelev/bos/chatmovies/di/KoinModule.kt
@@ -0,0 +1,14 @@
+package com.metelev.bos.chatmovies.di
+
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+val viewModelModule = module {
+}
+
+val retrofitModule = module {
+
+}
